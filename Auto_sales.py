1cdf752032ecf3ec6caced668733705110e6db79
GeritoJavita <geronimo.garciaalarcon@gmail.com>
2026-02-25 19:08:12 -0500

    realizando la configuracion

--- Auto_sales.py
+++ Auto_sales.py
@@ -8,3 +8,10 @@ from pynput import mouse, keyboard
 import psutil
 import pygetwindow as gw
 
+#Config
+
+Tiempo_Inactivo = 15 #Estos van por segundos
+Ultima_Actividad = time.time()
+waifu_window = None
+
+#Detector de actividad
